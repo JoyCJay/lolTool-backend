064b99aac6cd33731b32e9868c837d538fcc448c
uttxhy <haoyangxu123@gmail.com>
2019-05-14 15:39:58 +0200

    2019/05/14 在ConsultController中加入允许跨域的注解

--- src/main/java/com/example/demo/controller/ConsultController.java
+++ src/main/java/com/example/demo/controller/ConsultController.java
@@ -10,23 +10,21 @@ import com.example.demo.domain.Summoner;
 import com.example.demo.service.SummonerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * consultController
  */
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/consult")
-public class consultController {
+public class ConsultController {
 
 	private SummonerService summonerService;
 
 	@Autowired
-	consultController(SummonerService summonerService) {
+	ConsultController(SummonerService summonerService) {
 		this.summonerService = summonerService;
 	}
 
